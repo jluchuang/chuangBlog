51ea7a000837bd19b214dafcd906ddd21ebf8979
chuang <jlu_chuang@163.com>
2015-08-01 22:06:59 +0800

    Add About me and some css
    N/A
    
    Signed-off-by: chuang <jlu_chuang@163.com>

--- chuangBlog/urls.py
+++ chuangBlog/urls.py
@@ -3,21 +3,35 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'chuang.views.index', name='home'),
-    url(r'^chuangHome/$', 'chuang.views.chuangHome', name='chuangHome'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^jsonTest/$', 'chuang.views.jsonTest', name = 'jsonTest'),
-
-    url(r'^qrcode/$', 'tools.views.generate_qrcode', name = 'qrcode'), 
-
-    url(r'^admin/', include(admin.site.urls)),
-
+urlpatterns = patterns('', 
+    #For the common sources 
     url(r'^css/(?P<path>.*)$','django.views.static.serve', 
-    	{'document_root':'/home/chuang/chuangBlog/chuang/templates/css'}),
+    	{'document_root':'/home/chuang/chuangBlog/common/css'}),
 
     url(r'^js/(?P<path>.*)$','django.views.static.serve', 
-    	{'document_root':'/home/chuang/chuangBlog/chuang/templates/js'}),
+    	{'document_root':'/home/chuang/chuangBlog/common/js'}),
+
+    url(r'^inc/(?P<path>.*)$','django.views.static.serve', 
+    	{'document_root':'/home/chuang/chuangBlog/common/inc'}),
+
+    url(r'^media/(?P<path>.*)$','django.views.static.serve', 
+        {'document_root':'/home/chuang/chuangBlog/common/media'}),
+    
+    #Home 
+    url(r'^$', 'blog.views.index', name='home'),
+
+    #About me
+    url(r'^aboutMe/$', 'blog.views.chuangHome', name='aboutMe'),
+
+    #About me
+    url(r'^friends/$', 'blog.views.friends', name='friends'),
+
+    #Admin
+    url(r'^admin/', include(admin.site.urls)),
+
+    #Tools
+    url(r'^qrcode/$', 'tools.views.generate_qrcode', name = 'qrcode'),
+
+    #Just some study
+    url(r'^jsonTest/$', 'blog.views.jsonTest', name = 'jsonTest'),
 )
