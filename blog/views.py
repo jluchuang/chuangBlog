eb8d7f7e2122e53b921ea95c1b1202b9287e35be
chuang <jlu_chuang@163.com>
2015-08-01 22:06:59 +0800

    Add About me and some css
    N/A
    
    Signed-off-by: chuang <jlu_chuang@163.com>

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
+#coding : utf-8
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+import json
+
+def index(request) : 
+	return render(request, 'chuangHome.html')
+
+def chuangHome(request):
+	return render(request, 'chuangHome.html')
+
+def jsonTest(request):
+	List = ['find', 'study', 'try', 'work']
+	Dict = {'site': 'keeptry.cn', 'author': 'chuang'}
+	return render(request, 'jsonTest.html', { 
+		'List' : json.dumps(List), 
+		'Dict' : json.dumps(Dict)
+	    })
+
+def friends(request):
+	return render(request, 'friends.html')
