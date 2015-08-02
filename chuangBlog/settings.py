f84dc7fb4b0835d6e29af811619d7a24f4effd56
chuang <jlu_chuang@163.com>
2015-08-02 11:24:45 +0800

    Change the database settings into mysql
    N/A
    
    Signed-off-by: chuang <jlu_chuang@163.com>

--- chuangBlog/settings.py
+++ chuangBlog/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'chuangBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+#Database for mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'lu1990519',
+        'HOST': '',    #localhost
+        'PORT': '3306', 
     }
 }
 
